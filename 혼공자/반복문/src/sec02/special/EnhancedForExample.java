d1c932805f532fd3f7f4f2d9404b2192da85de80
FeelingXD <wlals425315@gmail.com>
2020-05-20 23:07:35 +0900

    혼공자 5-20

---
+++ 혼공자/반복문/src/sec02/special/EnhancedForExample.java
@@ -0,0 +1,17 @@
+package sec02.special;
+
+public class EnhancedForExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int [] num = {1,2,3,4,5};
+		
+		
+		for(int n :	num) {
+		System.out.println(n);
+		}// for( 자료형 변수 : 배열이름) 배열내용이 끝날때까지 반복
+		
+		}
+
+}
